0564e945a4e4b3b68d1bb1ff1a8de0f2001caa0b
Md.Ruhul Amin <135340696+mdruhulamin5347@users.noreply.github.com>
2024-08-11 02:46:54 +0600

    Update views.py

--- home/views.py
+++ home/views.py
@@ -31,17 +31,15 @@ def start_quiz(request, quiz_id):
 
 @login_required
 def submit_quiz(request, quiz_id):
-    # Check if the user has already submitted the quiz
     if request.session.get(f'quiz_submitted_{quiz_id}', False):
         messages.error(request, "You have already submitted this quiz.")
-        return redirect('quiz_list')  # Redirect to the quiz list or another page
-    # Check if the start time is set and if the time limit has been exceeded
+        return redirect('quiz_list') 
     start_time = request.session.get(f'quiz_start_time_{quiz_id}')
     if start_time:
-        start_time = timezone.make_aware(start_time)  # Ensure the time is timezone-aware
+        start_time = timezone.make_aware(start_time) 
         if timezone.now() > start_time + timedelta(minutes=2):
             messages.error(request, "Time limit exceeded. Quiz submission is not allowed.")
-            return redirect('quiz_list')  # Redirect to the quiz list or another page
+            return redirect('quiz_list')  
 
     if request.method == 'POST':
         quiz = get_object_or_404(Quiz, pk=quiz_id)
@@ -54,28 +52,23 @@ def submit_quiz(request, quiz_id):
                 submitted_answer = get_object_or_404(Answer, pk=submitted_answer_id)
                 if submitted_answer.is_correct:
                     score += 1
-
-        # Calculate the percentage score
+        
         total_questions = questions.count()
         percentage_score = (score / total_questions) * 100 if total_questions > 0 else 0
-
-        # Save the quiz score to the database
+        
         user = request.user
         quiz_score = QuizScore(user=user, quiz=quiz,total_question = total_questions, score=score, percentage_score=percentage_score)
         quiz_score.save()
-
-        # Mark the quiz as submitted in the session
+        
         request.session[f'quiz_submitted_{quiz_id}'] = True
-
-        # Render the results page
+    
         return render(request, 'quiz/quiz_result.html', {
             'quiz': quiz,
             'questions': questions,
             'score': score,
             'percentage_score': percentage_score,
-            'start_time': timezone.now(),  # Pass the start time to the template
+            'start_time': timezone.now(),  
 
         })
-    
-    # If request method is not POST, redirect to quiz list
+
     return redirect('quiz_list')
