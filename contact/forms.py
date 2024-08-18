a1f944ecb19124f32c14c147e619f68b004bd157
Md.Ruhul Amin <mdruhulamin534793@gmail.com>
2024-08-18 23:48:17 +0600

    all update project

--- contact/forms.py
+++ contact/forms.py
@@ -8,7 +8,13 @@ class UserCreateForm(UserCreationForm):
         model=User
         fields=('username','first_name','last_name','email')
     
+
 class ContactForm(forms.ModelForm):
     class Meta:
-        model=ContactModel
-        fields = '__all__'
+        model = ContactModel
+        fields = ['name', 'email', 'message']
+        
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['name'].required = True
+        self.fields['email'].required = True
